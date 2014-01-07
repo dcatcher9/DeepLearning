e407b5b624ebe887b9040c18c1ed86ca48912879
Yang Qu <dcatcher.qu@gmail.com>
2014-01-07 23:16:13 +0800

    passup implementation

--- DeepLearning/DeepLearningLib/DeepModel.cpp
+++ DeepLearning/DeepLearningLib/DeepModel.cpp
@@ -5,21 +5,39 @@
 
 namespace deep_learning_lib
 {
-    DataLayer::DataLayer(int height, int width, int depth)
-        : data_(height * width * depth), data_view_(height, width, depth, data_)
+    using namespace concurrency;
+
+    DataLayer::DataLayer(int depth, int width, int height)
+        : data_(depth * width * height), data_view_(depth, width, height, data_)
     {
     }
 
 
-    ModelLayer::ModelLayer(int num_neuron, int neuron_height, int neuron_width, int neuron_depth)
-        : weights_(num_neuron * neuron_height * neuron_width * neuron_depth),
-        weight_view_(concurrency::extent<4>(
-        std::array<int, 4>{{ num_neuron, neuron_height, neuron_width, neuron_depth }}.data()), weights_)
+    ModelLayer::ModelLayer(int num_neuron, int neuron_depth, int neuron_width, int neuron_height)
+        : weights_(num_neuron * neuron_depth * neuron_width * neuron_height),
+        weight_view_(extent<4>(std::array<int, 4>{{ num_neuron, neuron_depth, neuron_width, neuron_height }}.data()), weights_)
     {
     }
 
     void ModelLayer::PassUp(const DataLayer& bottom_layer, DataLayer& top_layer) const
     {
-        
+        assert(top_layer.depth() == this->neuron_num());
+
+        // readonly
+        array_view<const float, 3> bottom_layer_data = bottom_layer.data_view_;
+        array_view<const float, 4> neuron_weights = weight_view_;
+        // writeonly
+        array_view<float, 3>& top_layer_data = top_layer.data_view_;
+        top_layer_data.discard_data();
+
+        // non-tiled version
+        parallel_for_each(top_layer_data.extent,
+            [=](index<3> idx) restrict(amp)
+        {
+            for (int neuron_index = 0; neuron_index < idx[0]; neuron_index++)
+            {
+
+            }
+        });
     }
 }
--- DeepLearning/DeepLearningLib/DeepModel.h
+++ DeepLearning/DeepLearningLib/DeepModel.h
@@ -8,14 +8,14 @@ namespace deep_learning_lib
     // 3 dimension:
     //     _____________________
     //    /                    /|
-    //   /                    / |
+    //   / height             / |
     //  /       width        /  |
     //  ---------------------   |
     //  |                   |   |
-    //  | height            |  /
-    //  |                   | / depth
+    //  | depth             |  /
+    //  |                   | / 
     //  ---------------------/
-    //  height dimension is orderless, representing the concept of unordered set.
+    //  depth dimension is orderless, representing the concept of unordered set.
     class DataLayer
     {
     private:
@@ -26,7 +26,20 @@ namespace deep_learning_lib
         concurrency::array_view<float, 3> data_view_;
 
     public:
-        DataLayer(int height, int width, int depth);
+        DataLayer(int depth, int width, int height);
+
+        int depth() const
+        {
+            return data_view_.extent[0];
+        }
+        int width() const
+        {
+            return data_view_.extent[1];
+        }
+        int height() const
+        {
+            return data_view_.extent[2];
+        }
     };
 
     // Contains a collection of neurons, which is 3-dimensional according to data layer.
@@ -42,7 +55,24 @@ namespace deep_learning_lib
         concurrency::array_view<float, 4> weight_view_;
 
     public:
-        ModelLayer(int num_neuron, int neuron_height, int neuron_width, int neuron_depth);
+        ModelLayer(int num_neuron, int neuron_depth, int neuron_width, int neuron_height);
+
+        int neuron_num() const
+        {
+            return weight_view_.extent[0];
+        }
+        int neuron_depth() const
+        {
+            return weight_view_.extent[1];
+        }
+        int neuron_width() const
+        {
+            return weight_view_.extent[2];
+        }
+        int neuron_height() const
+        {
+            return weight_view_.extent[3];
+        }
 
         void PassUp(const DataLayer& bottom_layer, DataLayer& top_layer) const;
         void PassDown(const DataLayer& top_layer, DataLayer& bottom_layer) const;
