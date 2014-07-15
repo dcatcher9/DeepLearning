b59002bfbad27c15e2480c0a27d9a322e4469ecc
Yang Qu <dcatcher.qu@gmail.com>
2014-07-16 03:13:43 +0800

    implement top-freq style longterm memory training

--- DeepLearning/DeepLearningLib/AmpUtility.h
+++ DeepLearning/DeepLearningLib/AmpUtility.h
@@ -3,6 +3,7 @@
 #include <array>
 #include <tuple>
 #include <limits>
+#include <type_traits>
 
 #include <amp.h>
 
@@ -46,28 +47,82 @@ namespace deep_learning_lib
     template<typename T, int Rank>
     inline std::pair<concurrency::index<Rank>, T> min(concurrency::array_view<T, Rank>& arr)
     {
-        T minValue = std::numeric_limits<T>::lowest();
+        typedef typename std::remove_const<T>::type TT;
+
+        concurrency::array_view<TT> min_value_view(1);
+        min_value_view(0) = std::numeric_limits<TT>::max();
+        
+        concurrency::parallel_for_each(arr.extent, [=](concurrency::index<Rank> idx) restrict(amp)
+        {
+            TT old_min_value = min_value_view(0);
+            TT new_min_value;
+            T current_value = arr[idx];
+            do
+            {
+                new_min_value = current_value < old_min_value ? current_value : old_min_value;
+            } while (!concurrency::atomic_compare_exchange(
+                reinterpret_cast<unsigned int*>(&min_value_view(0)),
+                reinterpret_cast<unsigned int*>(&old_min_value),
+                (*(reinterpret_cast<unsigned int*>(&new_min_value)))));
+        });
+
+        concurrency::array_view<int> min_index_view(1);// used to communicate the min index back
+        concurrency::parallel_for_each(arr.extent, [=](concurrency::index<Rank> idx) restrict(amp)
+        {
+            T current_value = arr[idx];
+            if (current_value == min_value_view(0))
+            {
+                int old_min_index = min_index_view(0);
+                int new_min_index;
+                // flattern the index, take the smallest upon a draw
+                int current_index = idx[0];
+                for (int i = 1; i < Rank; ++i)
+                {
+                    current_index = current_index * arr.extent[i] + idx[i];
+                }
+                do
+                {
+                    new_min_index = current_index < old_min_index ? current_index : old_min_index;
+                } while (!concurrency::atomic_compare_exchange(
+                    reinterpret_cast<unsigned int*>(&min_index_view(0)),
+                    reinterpret_cast<unsigned int*>(&old_min_index),
+                    (*(reinterpret_cast<unsigned int*>(&new_min_index)))));
+            }
+        });
+
+        T min_value = min_value_view(0);
+        int min_flat_index = min_index_view(0);
+        concurrency::index<Rank> min_index;
+        for (int i = Rank - 1; i >= 0; --i)
+        {
+            min_index[i] = min_flat_index % arr.extent[i];
+            min_flat_index = (min_flat_index - min_index[i]) / arr.extent[i];
+        }
+        
+        assert(arr[min_index] == min_value);
+
+        return std::make_pair(min_index, min_value);
     }
 
     // until c++14, we cannot use initializer_list.size to remove the dependency on size template parameter
     inline concurrency::extent<4> make_extent(int e0, int e1, int e2, int e3)
     {
-        return concurrency::extent<4>(std::array<int, 4>{{e0, e1, e2, e3}}.data());
+        return concurrency::extent<4>(std::array < int, 4 > {{e0, e1, e2, e3}}.data());
     }
 
     inline concurrency::index<4> make_index(int e0, int e1, int e2, int e3)
     {
-        return concurrency::index<4>(std::array<int, 4>{{e0, e1, e2, e3}}.data());
+        return concurrency::index<4>(std::array < int, 4 > {{e0, e1, e2, e3}}.data());
     }
 
     inline concurrency::extent<5> make_extent(int e0, int e1, int e2, int e3, int e4)
     {
-        return concurrency::extent<5>(std::array<int, 5>{{e0, e1, e2, e3, e4}}.data());
+        return concurrency::extent<5>(std::array < int, 5 > {{e0, e1, e2, e3, e4}}.data());
     }
 
     inline concurrency::index<5> make_index(int e0, int e1, int e2, int e3, int e4)
     {
-        return concurrency::index<5>(std::array<int, 5>{{e0, e1, e2, e3, e4}}.data());
+        return concurrency::index<5>(std::array < int, 5 > {{e0, e1, e2, e3, e4}}.data());
     }
 
     inline int max(const int a, const int b) restrict(amp)
